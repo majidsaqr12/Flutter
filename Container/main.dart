8660498160bd56fdbe5ee40371fe1052cfd889e8
Majid Saqr <104238392+majidsaqr12@users.noreply.github.com>
2023-02-03 00:49:32 +0200

    Update main.dart

--- Container/main.dart
+++ Container/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:layout/ui/container.dart';
+import 'package:layout/ui/container.dart'; // package: projectNane /folderName /fileName.dart;
 void main() {
   runApp(const MyApp());
 }
